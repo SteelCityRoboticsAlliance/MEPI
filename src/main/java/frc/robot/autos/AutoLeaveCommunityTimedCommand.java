3e871ead00692459ad07bb939f2c6c3d7c71c29d
gos-thinkpad-3 <weiashley.gos@gmail.com>
2023-08-04 09:00:50 -0400

    created a timed auto command + fixed errors

--- src/main/java/frc/robot/autos/AutoLeaveCommunityTimedCommand.java
+++ src/main/java/frc/robot/autos/AutoLeaveCommunityTimedCommand.java
@@ -2,7 +2,6 @@ package frc.robot.autos;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutoDriveCommand;
-import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class AutoLeaveCommunityTimedCommand extends SequentialCommandGroup {
--- src/main/java/frc/robot/commands/AutoDriveCommand.java
+++ src/main/java/frc/robot/commands/AutoDriveCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 /** An example command that uses an example subsystem. */
